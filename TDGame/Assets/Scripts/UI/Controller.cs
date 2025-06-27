508d99099bbb1c4a12f1e249e2f67f2ab2347c89
christian460 <64232962+christian460@users.noreply.github.com>
2025-06-27 01:08:39 -0500

    Cambio al icono mute

--- TDGame/Assets/Scripts/UI/Controller.cs
+++ TDGame/Assets/Scripts/UI/Controller.cs
@@ -3,20 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class Controller : MonoBehaviour
 {
-	public AudioSource backgroundAudio;
-    
-	public void CambiarEscena(string nombre)
-	{
-		SceneManager.LoadScene(nombre);
-	}
-	
-	public void Mute()
-	{
-		if(backgroundAudio != null)
-		{
-			backgroundAudio.mute = !backgroundAudio.mute;
-		}
-	}
-	
-	
+    public AudioSource backgroundAudio;
+    public GameObject iconoSonido; 
+    public GameObject iconoMute;   
+
+    public void CambiarEscena(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+
+    public void Mute()
+    {
+        if (backgroundAudio != null)
+        {
+            backgroundAudio.mute = !backgroundAudio.mute;
+            bool estaMuteado = backgroundAudio.mute;
+            if (iconoSonido != null) iconoSonido.SetActive(!estaMuteado);
+            if (iconoMute != null) iconoMute.SetActive(estaMuteado);
+        }
+    }
 }
