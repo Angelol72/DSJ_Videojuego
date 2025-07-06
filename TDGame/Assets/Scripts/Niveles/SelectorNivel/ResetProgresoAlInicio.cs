edaad959ce378a6aae8d94dde42fa64b99b24b52
asantiagoq <91225951+SalomeYanarico@users.noreply.github.com>
2025-07-06 15:07:23 -0500

    Arreglando conflictos del nivel 1

--- TDGame/Assets/Scripts/Niveles/SelectorNivel/ResetProgresoAlInicio.cs
+++ TDGame/Assets/Scripts/Niveles/SelectorNivel/ResetProgresoAlInicio.cs
@@ -4,13 +4,8 @@ public class ResetProgresoAlInicio : MonoBehaviour
 {
     private void Awake()
     {
-        // Esto solo se ejecuta cuando entras por primera vez al juego
-        if (!PlayerPrefs.HasKey("progresoInicializado"))
-        {
-            PlayerPrefs.DeleteKey("nivelesDesbloqueados"); // Opcional: o ponlo a 1
-            PlayerPrefs.SetInt("nivelesDesbloqueados", 1); // Solo botón Fácil habilitado
-            PlayerPrefs.SetInt("progresoInicializado", 1); // Ya no lo volverá a hacer
-            PlayerPrefs.Save();
-        }
+        // Siempre se reinicia el progreso a 1 (solo nivel fácil habilitado)
+        PlayerPrefs.SetInt("nivelesDesbloqueados", 1);
+        PlayerPrefs.Save();
     }
 }
