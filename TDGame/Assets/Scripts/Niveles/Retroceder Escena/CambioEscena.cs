5895fe949a269d32114e3270846eb361431c2b6b
MarcoCarpioR <82545915+MarcoCarpioR@users.noreply.github.com>
2025-07-10 15:03:07 -0500

    Seleccion de niveles boton

--- TDGame/Assets/Scripts/Niveles/Retroceder Escena/CambioEscena.cs
+++ TDGame/Assets/Scripts/Niveles/Retroceder Escena/CambioEscena.cs
@@ -3,16 +3,16 @@ using UnityEngine.SceneManagement;
 public class CambioEscena : MonoBehaviour
 {
     public string nombreEscenaDestino = "Choice";
-    private AudioSource audioSource;
+    //private AudioSource audioSource;
 
  
 
     public void ReproducirSonidoYCambiarEscena()
     {
-        if (audioSource != null && audioSource.clip != null)
+       /* if (audioSource != null && audioSource.clip != null)
         {
             audioSource.PlayOneShot(audioSource.clip);
-        }
+        }*/
 
         Invoke("CargarEscena", 0.3f); // espera para dejar sonar el audio
     }
